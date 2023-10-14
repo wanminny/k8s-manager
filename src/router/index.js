54371f6495597656749457869af80cffd4b66c6c
min.wan <wan.min@zxelec.com>
2023-10-14 18:09:54 +0800

    axios

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/main', //视图
+      component: () => import('../views/AboutView.vue'),
+      icon: "odometer", //图标
+      meta: {title:"概要", requireAuth: false}, //定义meta元数据
+    },
+    {
+      path: '/404',
+      component: () => import('../views/common/404.vue'),
+      meta: {title:"404",requiredAuth:true},
+    },
+    {
+      path: '/:pathMatch(.*)',
+      redirect: '/404',
+    },
   ]
 })
 
