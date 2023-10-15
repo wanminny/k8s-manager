44f9cebd5b0188b2d41a7fe8e3f0c951efc00926
min.wan <wan.min@zxelec.com>
2023-10-15 13:53:06 +0800

    all user 遍历

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="about">
-    <h2 v-if="userData.data">{{ userData.data }}</h2>
+    <h2 v-if="userData.data">
+      <div>
+        {{ userData.data.id }} -- {{ userData.data.user_name }}
+      </div> 
+      <ul>
+      </ul>
+    </h2>
     <p v-else>Loading...</p>
-  <div>
-  </div>
-
+    <h2 v-if="userData.alluser">
+      <!-- {{ userData.alluser }} -->
+      <div>
+            <ul v-for="u in userData.alluser.users" v-bind:key="u.id">
+               {{ u.id }}--{{u.user_name}}
+            </ul>
+      </div> 
+    </h2>
+    <p v-else>Loading11...</p>
   </div>
 </template>
 
@@ -12,7 +24,8 @@
 import axiosInstance from '../utils/request'
 import { reactive } from 'vue';
 const userData = reactive({
-  data: '',
+  data: [],
+  alluser: [],
 });
 
 const fetchData  = (async () => {
@@ -22,23 +35,54 @@ const fetchData  = (async () => {
       url: '/v1/user/userid/4',
     });
     userData.data = response;
-    console.log(response);
   } catch (error) {
     console.error(error);
-    userData.data = null;
+    userData.data = [];
   }
 });
 
+const fetchAllData  = (async () => {
+  try {
+    const response = await axiosInstance({
+      method: 'get',
+      url: '/v1/user/users',
+    });
+    userData.alluser = response;
+  } catch (error) {
+    console.error(error);
+    userData.alluser = [];
+  }
+});
 fetchData()
+fetchAllData()
+console.log(userData.data);
 
 </script>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: 50vh;
     display: flex;
     align-items: center;
+    justify-content: center; /* 添加水平居中对齐 */
+    flex-direction: column; /* 垂直居中布局 */
+    text-align: center; /* 文字居中对齐 */
   }
+  .user {
+    min-height: 50vh;
+    display: flex;
+    align-items: center;
+    justify-content: center; /* 添加水平居中对齐 */
+    text-align: center; /* 文字居中对齐 */
+  }
+}
+/* 调整列表样式 */
+ul {
+  padding: 0;
+  list-style: none;
+}
+li {
+  margin: 10px 0;
 }
 </style>
