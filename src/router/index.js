e3138a140012ebaaa366024cb98ff99d45311175
min.wan <wan.min@zxelec.com>
2023-10-27 11:31:24 +0800

    删除干净

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import Layout from '../view/layout/lay-out.vue';
-
-// import {
-//   Home,
-// } from '@/page';
-
-// const pages = import.meta.glob('../view/page/**/*.vue');
-// const routes = Object.keys(pages).map((path)=>{
-//     let name = path.match(/\/page(.*)\.vue$/)[1].toLowerCase();
-//     console.log(1,name)
-//     // if(name.substring(name.length - 3) == 'mng'){
-//     //     name = name.slice(0, -5);//去掉最后的index
-//     //     console.log(2,name)
-//     // } 
-//     return {
-//         path: name === '/mng/home' ? '/': name,
-//         component: pages[path]
-//     }
-// })
-
 const router = createRouter({
   // 所有path 添加mng前缀;
   history: createWebHistory(import.meta.env.BASE_URL+ 'mng'),
-  
   routes: [
     {
       path: '/',
