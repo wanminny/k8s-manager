a0b5017de8674535470ca184c15301156b1ee468
min.wan <wan.min@zxelec.com>
2023-10-30 10:19:04 +0800

    添加请求拦截

--- src/utils/request.js
+++ src/utils/request.js
@@ -13,7 +13,15 @@ const service = axios.create(
     }
 );
 
-// 2. 响应拦截器
+//2. 请求拦截器
+service.interceptors.request.use(config => {
+    return config;
+  }, error => {
+    Promise.reject(error);
+  });
+
+  
+// 3. 响应拦截器
 service.interceptors.response.use(
 response => {
   //判断code码
